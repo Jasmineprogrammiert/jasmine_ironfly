ac19541409fd9c2375ccae3afa21ea752e50dfe8
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2024-01-26 17:02:16 +0800

    Minesweeper

---
+++ my-app/src/components/Cell.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Cell = ({ value, onClick, cMenu }) => {
+  const getValue = () => {
+    if (!value.isRevealed) {
+      return value.isFlagged ? "🚩" : null;
+    } else if (value.isMine) {
+      return "💣";
+    } else if (value.isEmpty) {
+      return "";
+    }
+
+    return value.n;
+  };
+
+  const className = `cell ${
+    value.isRevealed ? "" : "hidden"
+  } ${value.isMine ? "is-mine" : ""} ${
+    value.isClicked ? "is-clicked" : ""
+  } ${value.isEmpty ? "is-empty" : ""} ${
+    value.isUnknown ? "is-unknown" : ""
+  } ${value.isFlagged ? "is-flag" : ""}`;
+
+  return (
+    <div className={className} onClick={onClick} onContextMenu={cMenu}>
+      {getValue()}
+    </div>
+  );
+};
+
+const cellItemShape = {
+  x: PropTypes.number,
+  y: PropTypes.number,
+  n: PropTypes.number,
+  isRevealed: PropTypes.bool,
+  isMine: PropTypes.bool,
+  isFlagged: PropTypes.bool
+};
+
+Cell.propTypes = {
+  value: PropTypes.shape(cellItemShape),
+  onClick: PropTypes.func,
+  cMenu: PropTypes.func
+};
+
+export default Cell;
